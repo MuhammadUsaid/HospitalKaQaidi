7738507a779e3d5633b6a8430ae7f44857cb55a3
MuhammadUsaid <mohammadusaid40@gmail.com>
2018-11-17 12:01:38 +0500

    Deleted and Started Again

--- Project/include/Point.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Point
-{
-    int x;
-    int y;
-    Point();
-    Point(int x, int y);
-};
--- Project/include/Screen.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class Screen
-{
-public:
-    Screen();
-    virtual void Render(SDL_Renderer*);
-};
