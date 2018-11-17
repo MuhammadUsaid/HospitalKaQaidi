ebe7687d8188a8beab7e8a70af5e680405682deb
MuhammadUsaid <mohammadusaid40@gmail.com>
2018-11-17 11:19:43 +0500

    Initial Files Of Character, Word, LoadTexture and Button

--- Project/include/Character.h
+++ Project/include/Character.h
@@ -1,17 +1,25 @@
 #pragma once
-#include "LoadTexture.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include"LoadTexture.h"
+#include"Point.h"
 
 class Character
 {
-    Point position;
-    SDL_Rect charRect;
-    char shownChar;
-    LoadTexture* CharTexture;
+private:
+    Point position; //Position of the Character
+    int character_value = 0; //ascii value of the Character
+    int width; //width of character
+    int height; //height of character
+    SDL_Rect spriteClips; //clip specific to the Character
+    LoadTexture* spriteSheetTexture; //font image
 public:
     Character();
-    Character(char, LoadTexture*);
-    void Render(SDL_Renderer*);
-    void SetPosition(Point&);
-    void SetChar(char);
-    void SetTexture(LoadTexture*, char);
+    Character(LoadTexture* image, float x, float y, int ascii);
+    ~Character();
+    void Render(SDL_Renderer* gRenderer, bool debug);
+    void operator = (const Character& cpy); //operator overloading for assignment operatot
 };
+
--- Project/include/Word.h
+++ Project/include/Word.h
@@ -1,17 +1,27 @@
 #pragma once
+
 #include "LoadTexture.h"
-#include "Point.h"
+#include "Character.h"
 
 class Word
 {
-    Point position;
-    char* renderWord;
-    LoadTexture* textTexture;
-    Character* characters;
+private:
+    Point position; //position of word
+    std::string text; //text of the word
+    int wordLen;  //length of the word
+    int width;   //width of the word
+    int height; //height of the word
+    LoadTexture* spriteSheetTexture;
+    Character* chars; //pointer of character class(used for creating dynamic array of Characters)
+
 public:
-    Word(string, Loadtexture*)
-    void Render(SDL_Renderer*);
-    void SetText(char*);
-    void SetPosition(Point&);
-    int GetTextlength();
+    Word();
+    Word(std::string, LoadTexture*, float, float );
+    ~Word();
+    void Render(SDL_Renderer*); //to draw the word on screen
+    int getLength();
+    int getWidth();
+    void changeWord(std::string);  //changes the text of the Word
+    void operator = (const Word&); //operating overloading for assignment operator.
+    std::string getText() {return text;} //used to get text of the word
 };
